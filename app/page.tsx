4ead53d8ed663774d9be2661c75e4b6a64bbae1a
wangs <wangs@cityfun.com.cn>
2026-02-11 16:40:28 +0800

    Refactor language selection dropdowns to use AutoComplete component with custom filter option.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Select, Input, message, Alert, Popover } from 'antd';
+import { Select, Input, message, Alert, Popover, AutoComplete } from 'antd';
 const { TextArea } = Input;
 import Link from 'next/link';
 import Image from 'next/image';
@@ -191,7 +191,7 @@ export default function Home() {
           <div className="grid mt-4 md:grid-cols-2 md:mx-0 md:gap-4 grid-cols-1 gap-0 mx-4">
             <div>
               <div className='flex flex-row'>
-                <Select
+                <AutoComplete
                   defaultValue="Auto"
                   value={selectedFromLanguage}
                   size='large'
@@ -209,13 +209,17 @@ export default function Home() {
                     { value: 'German', label: l('german') },
                     { value: 'Spanish', label: l('spanish') },
                   ]}
+                  filterOption={(inputValue, option) =>
+                    option!.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1 ||
+                    option!.label.toString().toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                  }
                 />
                 <Button
                   className='flex-grow-0 m-2'
                   type='text'
                   onClick={swapLanguage}
                   icon={<SwapOutlined style={{ color: '#ccc', }} />} size='small' />
-                <Select
+                <AutoComplete
                   defaultValue="Simplified Chinese"
                   value={selectedToLanguage}
                   id='toLanguage'
@@ -232,6 +236,10 @@ export default function Home() {
                     { value: 'German', label: l('german') },
                     { value: 'Spanish', label: l('spanish') },
                   ]}
+                  filterOption={(inputValue, option) =>
+                    option!.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1 ||
+                    option!.label.toString().toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                  }
                 />
               </div>
               <div className='mt-4'>
